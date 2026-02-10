cd876beb07aaad06fd01929f58c08ca893a16dbd
mildman1848 <215402881+mildman1848@users.noreply.github.com>
2026-02-10 19:55:07 +0100

    fixing links

--- ct/audiobookshelf.sh
+++ ct/audiobookshelf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/asylumexp/Proxmox/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/mildman1848/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2026 tteck
 # Author: tteck (tteckster)
 # License: MIT | https://github.com/asylumexp/Proxmox/raw/main/LICENSE
